f8d90f50f482fd9933204864ac80743ac63fe70a
martina <martina.bevilacqua1992@gmail.com>
2019-12-13 17:03:33 +0100

    bugged indexes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,23 +12,24 @@ export class AppComponent {
   title = 'test-sqv';
   chars = {};
   sqv;
+  exOne;
   options = {
 
     mode: 'viewSequence',
 
     parameters: {
       fontSize: '14px',
-      chunkSize: 5,
+      chunkSize: 10,
       skipIndex: 5,
       colorChoice: 'opposite'
     },
 
     view: [
-      'MFVPETEDMLPRLAPRPSAAVPMGHTNEIIGPTVPEVSILFGQPPQDPQMQPQQPNHAHQESSPRAAPTFASWKKQMLPRVNFSPILATELGPRTMFVPETEDMLPRLAPRPS',
+      'MFVPETEDMLPRLAPRPSAAVPMGHTNEIIGPTVPEVSILFGQPPQDPQMQPQQPN',
       {2: 'a', 3: 'a', 4: 'a', 5 : 'a', 6: 'a', 7: 'a', 8 : 'a',  40: 'b', 41: 'b',
         42: 'b', 43: 'b', 44: 'b', 93: 'a', 94: 'a'},
-      {0: '&#x2605;', 1 : '&#x2605;', 31: '&#x2605;', 32 : '&#x2605;', 60: '&#x2605;',  	61: '&#x2605;'},
-      {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 10: 11, 11: 12, 12: 13, 13: 14, 	14: 15, 15: 16, 92: 93, 93: 94}
+      {0: '&#x2605;', 1 : '&#x2605;', 31: '&#x2605;', 32 : '&#x2605;', 51: '&#x2605;',  52: '&#x2605;'},
+      {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 10: 11, 11: 12, 12: 13, 13: 14, 	14: 15, 15: 16}
     ],
 
     colors: [
@@ -46,8 +47,8 @@ export class AppComponent {
       },
       {
         type: 'region',
-        start: 53,
-        end: 59,
+        start: 45,
+        end: 50,
         color: {r: 0, g: 102, b: 255, a: 0.4}
       },
       {
@@ -64,15 +65,18 @@ export class AppComponent {
 
     parameters: {
       fontSize: '14px',
-      chunkSize: 5,
+      chunkSize: 20,
       skipIndex: 5,
       colorChoice: 'opposite'
     },
 
     view: [
-      'MFVPETEDMLPRLAPRPSAAVPMGHA',
-      'MFVPETEDMLPRLAPRPSAAVPMGHA',
-      'MFVPETEDMLPRLAPRPSAAVPMGHA',
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA',
       {2: 'a', 3: 'a', 4: 'a', 5 : 'a', 6: 'a', 7: 'a', 8 : 'a',  40: 'b', 41: 'b',
         42: 'b', 43: 'b', 44: 'b', 93: 'a', 94: 'a'},
       {0: '&#x2605;', 1 : '&#x2605;', 31: '&#x2605;', 32 : '&#x2605;', 60: '&#x2605;',  	61: '&#x2605;'},
@@ -98,53 +102,77 @@ export class AppComponent {
     ]
   };
 
+  exTwo;
 
 
+  runExOne() {
 
-  refresh() {
-    this.options = {
+    this.exOne =  {
 
       mode: 'viewSequence',
 
       parameters: {
-        fontSize: '14px',
-        chunkSize: 5,
-        skipIndex: 5,
         colorChoice: 'opposite'
       },
 
       view: [
-        'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG'
+        'MFVPETEDMLPRLAPRPSAAVPMGHTNEIIGPTVPEVSILFGQPPQDPQM'
       ],
 
       colors: [
         {
           type: 'region',
           start: 1,
-          end: 2,
-          color: {r: 204, g: 255, b: 0}
+          end: 18
         },
         {
           type: 'region',
-          start: 3,
-          end: 15,
-          color: {r: 0, g: 102, b: 255, a: 0.4}
-        },
-        {
-          type: 'region',
-          start: 20,
-          end: 22,
-          color: {r: 0, g: 102, b: 255, a: 0.4}
-        },
-        {
-          type: 'region',
-          start: 23,
-          end: 25,
-          color: {r: 0, g: 102, b: 25, a: 0.4}
+          start: 19,
+          end: 27
         }
       ]
     };
     this.sqv = new SqvLibComponent();
-    this.sqv.draw(this.options);
+    this.sqv.draw(this.exOne);
+  }
+
+  runExTwo() {
+
+    this.exTwo = {
+
+      mode: 'viewSequence',
+
+      parameters: {
+        colorChoice: 'opposite',
+        chunkSize: 10
+      },
+
+      view: [
+        'MFVPETEDMLPRLAPRPSAAVPMGHTNEIIGPTVPEVSILFGQPPQDPQM',
+      ],
+
+      colors: [
+        {
+          type: 'region',
+          start: 1,
+          end: 5,
+          color: {r: 204, g: 255, b: 0}
+        },
+        {
+          type: 'region',
+          start: 6,
+          end: 8,
+          color: {r: 204, g: 255, b: 0}
+        },
+        {
+          type: 'region',
+          start: 9,
+          end: 12,
+          color: {r: 204, g: 255, b: 0}
+        }
+      ]
+    };
+    this.sqv = new SqvLibComponent();
+    this.sqv.draw(this.exOne);
   }
 }
