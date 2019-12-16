4fa3558c104259dcb7a1d1a5df58e2108eb69aa4
martina <martina.bevilacqua1992@gmail.com>
2019-12-16 17:30:39 +0100

    summary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -198,4 +198,81 @@ export class AppComponent {
       ],
       id: 'sqv-colo'
     };
+
+  alignSimple = {
+
+    mode: 'alignSequences',
+
+    parameters: {
+      colorChoice: 'opposite'
+    },
+
+    view: [
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA'
+    ],
+
+    colors: [
+      {
+        type: 'aminoacid',
+        letter: 'A',
+        color: {r: 204, g: 255, b: 0, a: 0.4}
+      },
+      {
+        type: 'aminoacid',
+        letter: 'D',
+        color: {r: 0, g: 255, b: 102, a: 0.4}
+      },
+      {
+        type: 'aminoacid',
+        letter: 'V',
+        color: {r: 239, g: 100, b: 97, a: 0.4}
+      },
+      {
+        type: 'aminoacid',
+        letter: 'P',
+        color: {r: 144, g: 0, b: 179, a: 0.4}
+      }
+    ],
+    id: 'sqv-alS'
+  };
+
+  alignInd = {
+
+    mode: 'alignSequences',
+
+    parameters: {
+      chunkSize: 10,
+      skipIndex: 5,
+      colorChoice: 'opposite'
+    },
+
+    view: [
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA',
+      'MFVPETEDMLPRLAPRPSA',
+      {2: 'a', 3: 'a', 4: 'a', 5 : 'a', 6: 'a', 7: 'a', 8 : 'a',  40: 'b', 41: 'b',
+        42: 'b', 43: 'b', 44: 'b', 93: 'a', 94: 'a'},
+      {0: '&#x2605;', 1 : '&#x2605;', 31: '&#x2605;', 32 : '&#x2605;', 60: '&#x2605;',  	61: '&#x2605;'},
+      {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 10: 11, 11: 12, 12: 13, 13: 14, 	14: 15, 15: 16, 92: 93, 93: 94}
+    ],
+
+    colors: [
+      {
+        type: 'aminoacid',
+        letter: 'A',
+        color: {r: 204, g: 255, b: 0, a: 0.4}
+      },
+      {
+        type: 'aminoacid',
+        letter: 'D',
+        color: {r: 0, g: 255, b: 102, a: 0.4}
+      }
+    ],
+    id: 'sqv-alI'
+  };
+
+
 }
